ffe707dd2158e0d1e54c7b3dcd8263b02ea22af0
Soran <sss.soran20@gmail.com>
2025-08-09 16:23:38 +0330

    text

--- src/Commponents/Ui/Molecules/BoxRentText/BoxRentText.jsx
+++ src/Commponents/Ui/Molecules/BoxRentText/BoxRentText.jsx
@@ -4,7 +4,7 @@ function BoxRentText() {
   return (
     <div className="w-full flex items-center justify-center flex-col ">
       <Text style="text-[2.1vw] text-[#212121] font-bold " >سقفینو فرصتی برای همه</Text>
-      <Text style="text-[1.6vw] text-[#353535] font-bold " >اگر مالک یا در جست‌‌وجوی سقفی نو هستید، کلیک کنید</Text>
+      <Text style="text-[1.9vw] text-[#353535] font-bold " >اگر مالک یا در جست‌‌وجوی سقفی نو هستید، کلیک کنید</Text>
     </div>
   );
 }
