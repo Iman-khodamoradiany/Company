3abc5c6b922314cf8f6337ffd9aaa7434e6897ad
iman khodamoradiany <imankhodamoradiany@gmail.com>
2025-08-12 19:50:24 +0330

    BussinessCard

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { createContext, useState } from "react"
 import Layout from "./Commponents/Partial/Layout"
 import HomePage from "./Pages/HomePage/HomePage"
 import { Route, Routes } from "react-router"
-import Page2Homes from "./Pages/Page2Homes/Page2Homes"
 
 export const MyContext = createContext()
 
 function App() {
   const [PopUp, setPopUp] = useState(false)
   const [Token, setToken] = useState(false);
+  const [Card , setCard] = useState(false)
   return (
     <>
-      <MyContext.Provider value={{ PopUp, setPopUp, Token, setToken }}>
+      <MyContext.Provider value={{ PopUp, setPopUp, Token, setToken , Card , setCard}}>
         <Layout>
           <Routes>
             <Route path="/" element={<HomePage />} />
